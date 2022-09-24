52b5030c6cd9e053c84728c4bf88d404b87071aa
Clementweb3 <coderfanai77@gmail.com>
2022-09-24 23:15:52 +0530

    change screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,12 +33,6 @@ const Navbar = () => {
   ]
 
 
-  // <ul style={{display: 'flex', listStyle: 'none', justifyContent: 'space-around'}}>
-  // <li><Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link></li>
-  // <li><Link  to="about" spy={true} smooth={true}>About</Link></li>
-  // <li><Link  to="contact" spy={true} smooth={true}>Contact</Link></li>
-  // <li><Link  to="service" spy={true} smooth={true}>Service</Link></li>
-  //   </ul>
   return (
 
 
